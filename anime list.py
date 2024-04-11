06568943eec29afe2aa78796d03b6c5b82f3bff4
Shamilok <shmi1din@gmail.com>
2024-04-11 21:33:04 +0500

    Will Watch list added

--- anime list.py
+++ anime list.py
@@ -5,7 +5,7 @@ eel.init('web')
 anime_list = []
 
 @eel.expose
-def load_csv():
+def load_main_csv():
     anime_list.clear()
     try:
         with open('data.csv', newline='', encoding='utf-8') as csvfile:
@@ -17,25 +17,36 @@ def load_csv():
         pass
 
 @eel.expose
-def add_anime(newAnime):
-    load_csv()
-    anime_list.append(newAnime)
-    with open('data.csv', 'w', newline='', encoding='utf-8') as csvfile:
-        fieldnames = ['Name', 'Progress', 'ProgressType', 'Notes']
-        writer = csv.DictWriter(csvfile, fieldnames=fieldnames)
-        writer.writeheader()
-        for anime in anime_list:
-            writer.writerow(anime)
+def load_willWatch_csv():
+    anime_list.clear()
+    try:
+        with open('willwatch.csv', newline='', encoding='utf-8') as csvfile:
+            reader = csv.DictReader(csvfile)
+            for row in reader:
+                anime_list.append(row)
+            return anime_list
+    except FileNotFoundError:
+        pass
 
 @eel.expose
-def delete_anime(index):
+def add_anime(newAnime, isMain):
+    if(isMain):
+        load_main_csv()
+        anime_list.append(newAnime)
+        writeCSV('data.csv', isMain)
+    else:
+        load_willWatch_csv()
+        anime_list.append(newAnime)
+        writeCSV('willwatch.csv', isMain)
+    
+    
+@eel.expose
+def delete_anime(index, isMain):
     anime_list.pop(index)
-    with open('data.csv', 'w', newline='', encoding='utf-8') as csvfile:
-        fieldnames = ['Name', 'Progress', 'ProgressType', 'Notes']
-        writer = csv.DictWriter(csvfile, fieldnames=fieldnames)
-        writer.writeheader()
-        for anime in anime_list:
-            writer.writerow(anime)
+    if(isMain):
+        writeCSV('data.csv', isMain)
+    else:
+        writeCSV('willwatch.csv', isMain)
 
 @eel.expose
 def find_anime(name):
@@ -46,5 +57,13 @@ def find_anime(name):
             if re.search(name, row['Name'], re.IGNORECASE):
                 matching_anime.append(row)
         return matching_anime
+
+def writeCSV(CSVName, isMain):
+    with open(CSVName, 'w', newline='', encoding='utf-8') as csvfile:
+        fieldnames = ['Name', 'Progress', 'ProgressType', 'Notes'] if isMain else ['Name', 'Notes']
+        writer = csv.DictWriter(csvfile, fieldnames=fieldnames)
+        writer.writeheader()
+        for anime in anime_list:
+            writer.writerow(anime)
 
 eel.start('index.html', size=(1300, 900))
--- web/script.js
+++ web/script.js
@@ -1,49 +1,135 @@
-let animeName = document.getElementById("anime-name")
-let progress = document.getElementById("anime-progress")
-let notes = document.getElementById("anime-notes")
-let progressType = document.querySelectorAll('input[name="progressType"]')
-let animeListDiv = document.getElementById('anime-list');
-let addBtn =  document.getElementById('anim-add-btn');
-let search =  document.getElementById('search');
-let animeCount =  document.getElementById('anime-count');
-let progressTypePrev = 'сезоны'
-for (var i = 0; i < progressType.length; i++) {
-progressType[i].addEventListener('change', function() {
-    (progressTypePrev) ? console.log(progressTypePrev.value): null;
-    if (this !== progressTypePrev) {
-        progressTypePrev = this.value;
+const mainPage =  document.getElementById('main-page');
+const animeCount =  document.getElementById('anime-count');
+
+function loadMainPage(){
+    mainPage.innerHTML = 
+    `<div class="anime-text">Название</div>
+    <input type='text' class="input" id='anime-name' placeholder='Атака титанов'>
+    <div class="anime-text">Тип прогресса</div>
+    <div id='anime-radio'>
+        <input type="radio" id="season" name="progressType" value="сезоны" checked />
+        <label for="season">сезоны</label>
+        <input type="radio" id="series" name="progressType" value="серии" />
+        <label for="series">серии</label>
+        <input type="radio" id="film" name="progressType" value="фильм" />
+        <label for="film">фильм</label>
+        <input type="radio" id="nd" name="progressType" value="не применимо" />
+        <label for="nd">не применимо</label>
+    </div>
+    <div class="anime-text">Просмотрено</div>
+    <input class="input" type='number' id='anime-progress' placeholder='2'>
+    <div class="anime-text">Примечания</div>
+    <input class="input" type='text' id='anime-notes' placeholder='Класное аниме, плюс просмотрен фильм'>
+    <input class="input" type='button' id='anim-add-btn' value='Добавить' onclick="addAnime">
+    <input class="input" type='text' id='search' placeholder='Поиск'>
+    <div id="anime-list"></div>
+    <div id="willwatch">
+        <svg width="30px" height="30px" viewBox="0 0 24 24" fill="#ffffff">
+            <path d="M5 6.2C5 5.07989 5 4.51984 5.21799 4.09202C5.40973 3.71569 5.71569 3.40973 6.09202 3.21799C6.51984 3 7.07989 3 8.2 3H15.8C16.9201 3 17.4802 3 17.908 3.21799C18.2843 3.40973 18.5903 3.71569 18.782 4.09202C19 4.51984 19 5.07989 19 6.2V21L12 16L5 21V6.2Z" stroke="#ffffff" stroke-width="2" stroke-linejoin="round"/>
+        </svg>
+        <p>Смотреть позже</p>
+    </div>`
+    
+    const addBtn =  document.getElementById('anim-add-btn');
+    const search =  document.getElementById('search');
+    const willWatchBtn =  document.getElementById('willwatch');
+    const animeListDiv = document.getElementById('anime-list');
+    const progressType = document.querySelectorAll('input[name="progressType"]')
+    const animeName = document.getElementById("anime-name")
+    const progress = document.getElementById("anime-progress")
+    const notes = document.getElementById("anime-notes")
+    let progressTypePrev = 'сезоны'
+    addBtn.addEventListener('click', {handleEvent: addAnime, progressTypePrev: progressTypePrev, progress: progress, animeName: animeName, notes: notes, isMain: true, search: search, animeListDiv: animeListDiv, animeCount: animeCount})
+    search.addEventListener('input', {handleEvent: loadAnimeList, isSearch: true, search: search, animeListDiv: animeListDiv, isMain: true}) 
+    willWatchBtn.addEventListener('click', loadWillWatchPage)
+    
+    for (var i = 0; i < progressType.length; i++) {
+        progressType[i].addEventListener('change', function() {
+            if (this !== progressTypePrev) {
+                progressTypePrev = this.value;
+            }
+        })
     }
-})
+    loadAnimeList.call({ isSearch: false, animeListDiv: animeListDiv, isMain: true, animeCount: animeCount});
 }
 
-addBtn.addEventListener('click', addAnime)
-search.addEventListener('input', {handleEvent: loadAnimeList, isSearch: true}) // не смотри сюда
+function loadWillWatchPage() {
+    mainPage.innerHTML = 
+    `<div class="anime-text">Название</div>
+    <input type='text' class="input" id='anime-name' placeholder='Атака титанов'>
+    <div class="anime-text">Примечания</div>
+    <input class="input" type='text' id='anime-notes' placeholder='Заметки'>
+    <input class="input" type='button' id='anim-add-btn' value='Добавить' onclick="addAnime">
+    <input class="input" type='text' id='search' placeholder='Поиск'>
+    <div id="anime-list"></div>
+    <div id="willwatch">
+        <svg width="30px" height="30px" viewBox="0 0 24 24" fill="#ffffff">
+            <path d="M5 6.2C5 5.07989 5 4.51984 5.21799 4.09202C5.40973 3.71569 5.71569 3.40973 6.09202 3.21799C6.51984 3 7.07989 3 8.2 3H15.8C16.9201 3 17.4802 3 17.908 3.21799C18.2843 3.40973 18.5903 3.71569 18.782 4.09202C19 4.51984 19 5.07989 19 6.2V21L12 16L5 21V6.2Z" stroke="#ffffff" stroke-width="2" stroke-linejoin="round"/>
+        </svg>
+        <p>Просмотренные аниме</p>
+    </div>`
+
+    const addBtn =  document.getElementById('anim-add-btn');
+    const search =  document.getElementById('search');
+    const willWatchBtn =  document.getElementById('willwatch');
+    const animeListDiv = document.getElementById('anime-list');
+    const progressType = document.querySelectorAll('input[name="progressType"]')
+    const animeName = document.getElementById("anime-name")
+    const notes = document.getElementById("anime-notes")
+    let progressTypePrev = 'сезоны'
+    addBtn.addEventListener('click', {handleEvent: addAnime, progressTypePrev: progressTypePrev, isMain: false, search: search, animeListDiv: animeListDiv, animeName: animeName, notes: notes})
+    search.addEventListener('input', {handleEvent: loadAnimeList, isSearch: true, search: search, animeListDiv: animeListDiv, isMain: false})
+    willWatchBtn.addEventListener('click', loadMainPage)
+    
+    for (var i = 0; i < progressType.length; i++) {
+        progressType[i].addEventListener('change', function() {
+            if (this !== progressTypePrev) {
+                progressTypePrev = this.value;
+            }
+        })
+    }
+    loadAnimeList.call({ isSearch: false, animeListDiv: animeListDiv, isMain: false, animeCount: animeCount });
+}
 
 function addAnime() {
-    if (progressTypePrev === "не применимо" && progress.value != "")
+    if(this.isMain) {
+        if (this.progressTypePrev === "не применимо" && this.progress.value != "")
         return
         
-    if (progressTypePrev != "не применимо" && progress.value === "")
-        return
-    if(animeName.value){
-        eel.add_anime({
-            'Name': animeName.value, 
-            'Progress': progress.value ? progress.value : 'n/d', 
-            'ProgressType': progressTypePrev, 
-            'Notes': notes.value ? notes.value : 'n/d'
-        })()
-        animeName.value = ''
-        progress.value = ''
-        notes.value = ''
-        search.value = ''
-        loadAnimeList.call({ isSearch: false }); // сюда тоже не смотри
+        if (this.progressTypePrev != "не применимо" && this.progress.value === "")
+            return
+
+        if(this.animeName.value){
+            eel.add_anime({
+                'Name': this.animeName.value, 
+                'Progress': this.progress.value ? this.progress.value : 'n/d', 
+                'ProgressType': this.progressTypePrev, 
+                'Notes': this.notes.value ? this.notes.value : 'n/d'
+            }, this.isMain)()
+            this.animeName.value = ''
+            this.progress.value = ''
+            this.notes.value = ''
+            this.search.value = ''
+        }
+    } else {
+        if(this.animeName.value){
+            eel.add_anime({
+                'Name': this.animeName.value, 
+                'Notes': this.notes.value ? this.notes.value : 'n/d'
+            }, this.isMain)()
+            this.animeName.value = ''
+            this.notes.value = ''
+            this.search.value = ''
+        }
     }
     
+    loadAnimeList.call({ isSearch: false, animeListDiv: this.animeListDiv, isMain: this.isMain, animeCount: this.animeCount });
 }
-async function loadAnimeList(event) {
-    animeListDiv.innerHTML = ''
+
+async function loadAnimeList() {
+    this.animeListDiv.innerHTML = ''
     try {
-        let arr = this.isSearch ? await eel.find_anime(search.value)() : await eel.load_csv()();
+        let arr = this.isSearch ? await eel.find_anime(search.value)() : (this.isMain ? await eel.load_main_csv()() : await eel.load_willWatch_csv()());
         arr.reverse().forEach((anime, index) => {
             let animeItemDiv = document.createElement('div');
             animeItemDiv.classList.add('anime-list-item');
@@ -64,7 +150,7 @@ async function loadAnimeList(event) {
             deleteButton.textContent = 'X';
             deleteButton.classList.add('delete-button');
             deleteButton.addEventListener('click', (event) => {
-                eel.delete_anime(arr.length-index-1)()
+                eel.delete_anime(arr.length-index-1, this.isMain)()
                 event.target.parentElement.remove();
             });
 
@@ -72,7 +158,7 @@ async function loadAnimeList(event) {
             animeListItemWrapper.classList.add('anime-list-item-wrapper');
 
             animeListItemWrapper.appendChild(nameDiv);
-            if(anime.ProgressType != "не применимо") {
+            if(anime.ProgressType != "не применимо" && this.isMain) {
                 animeListItemWrapper.appendChild(progressDiv);
             }
             if(anime.Notes != "n/d") {
@@ -80,12 +166,13 @@ async function loadAnimeList(event) {
             }
             animeItemDiv.appendChild(animeListItemWrapper);
             animeItemDiv.appendChild(deleteButton);
-            animeListDiv.appendChild(animeItemDiv);
+            this.animeListDiv.appendChild(animeItemDiv);
         });
-        animeCount.innerText = this.isSearch && search.value != 0 ? `Всего найдено: ${arr.length} тайтлов` : `Всего добавлено: ${arr.length} тайтлов`
+        animeCount.innerText = this.isSearch && this.search.value != 0 ? `Всего найдено: ${arr.length} тайтлов` : `Всего добавлено: ${arr.length} тайтлов`
 
     } catch (error) {
         console.error(error);
     }
 }
-loadAnimeList.call({ isSearch: false }); // ...
+
+loadMainPage()
