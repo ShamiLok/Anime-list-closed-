3326dd68cfa84efbae52468a54997409e24a9235
Shamilok <shmi1din@gmail.com>
2024-06-08 20:49:51 +0500

    added server support, removed local saving

--- anime list.py
+++ anime list.py
@@ -1,69 +1,140 @@
-import csv, eel, re
+import csv, eel, re, os, requests, json
 
-eel.init('web')
+eel.init(f'{os.path.dirname(os.path.realpath(__file__))}/web')
 
 anime_list = []
+willWatch_list = []
+
+# загадка с функцией ниже
+# если был задан неверный адрес сервера, то функция ниже не будет вызыватся с js (loadSettingPage) пока load_main_csv не выдаст ошибку (где то 10 секунд)
+@eel.expose
+def getConfigDate(configName):
+    with open("config.json", "r") as config_file:
+        config_data = json.load(config_file)
+        return(config_data[configName])
+
+@eel.expose
+def setConfigDate(configName, newConfig):
+    with open("config.json", "r") as config_file:
+        config_data = json.load(config_file)
+    config_data[configName] = newConfig
+    with open("config.json", "w") as config_file:
+        json.dump(config_data, config_file)
+
+
+baseURL = getConfigDate("baseURL")
 
 @eel.expose
 def load_main_csv():
-    anime_list.clear()
     try:
-        with open('data.csv', newline='', encoding='utf-8') as csvfile:
-            reader = csv.DictReader(csvfile)
-            for row in reader:
-                anime_list.append(row)
-            return anime_list
-    except FileNotFoundError:
-        pass
+        anime_list.clear()
+        url = baseURL + "?type=main"
+        response = requests.get(url)
+        rows = response.text.split("\n")
+        for row in csv.DictReader(rows):
+            anime_list.append(row)
+        print("Содержимое файла CSV успешно обновлено")
+        return anime_list
+    except requests.exceptions.RequestException as e:
+        print(f"Ошибка при подключении к серверу: {e}")
+        return []
+
+# @eel.expose
+# def load_main_csv():
+#     anime_list.clear()
+#     try:
+#         with open('data.csv', newline='', encoding='utf-8') as csvfile:
+#             reader = csv.DictReader(csvfile)
+#             for row in reader:
+#                 anime_list.append(row)
+#             return anime_list
+#     except FileNotFoundError:
+#         pass
 
 @eel.expose
 def load_willWatch_csv():
-    anime_list.clear()
     try:
-        with open('willwatch.csv', newline='', encoding='utf-8') as csvfile:
-            reader = csv.DictReader(csvfile)
-            for row in reader:
-                anime_list.append(row)
-            return anime_list
-    except FileNotFoundError:
-        pass
+        willWatch_list.clear()
+        url = baseURL + "?type=willwatch"
+        response = requests.get(url)
+        rows = response.text.split("\n")
+        for row in csv.DictReader(rows):
+            willWatch_list.append(row)
+        print("Содержимое файла willwatch.csv успешно обновлено")
+        return willWatch_list
+    except requests.exceptions.RequestException as e:
+        print(f"Ошибка при подключении к серверу: {e}")
+        return []
 
+    # try:
+    #     with open('willwatch.csv', newline='', encoding='utf-8') as csvfile:
+    #         reader = csv.DictReader(csvfile)
+    #         for row in reader:
+    #             anime_list.append(row)
+    #         return anime_list
+    # except FileNotFoundError:
+    #     pass
+
+# @eel.expose
+# def add_anime(newAnime, isMain):
+#     if(isMain):
+#         load_main_csv()
+#         anime_list.append(newAnime)
+#         writeCSV('data.csv', isMain)
+#     else:
+#         load_willWatch_csv()
+#         will_watch_list.append(newAnime)
+#         writeCSV('willwatch.csv', isMain)
 @eel.expose
 def add_anime(newAnime, isMain):
-    if(isMain):
-        load_main_csv()
-        anime_list.append(newAnime)
-        writeCSV('data.csv', isMain)
-    else:
-        load_willWatch_csv()
-        anime_list.append(newAnime)
-        writeCSV('willwatch.csv', isMain)
-    
-    
+    try:
+        url = baseURL + "?type=" + ("main" if isMain else "willwatch")
+        response = requests.post(url, data=newAnime)
+    except requests.exceptions.RequestException as e:
+        print(f"Ошибка при подключении к серверу: {e}")
+        return []
+# @eel.expose
+# def delete_anime(index, isMain):
+#     anime_list.pop(index)
+#     if(isMain):
+#         writeCSV('data.csv', isMain)
+#     else:
+#         writeCSV('willwatch.csv', isMain)
+
 @eel.expose
 def delete_anime(index, isMain):
-    anime_list.pop(index)
-    if(isMain):
-        writeCSV('data.csv', isMain)
-    else:
-        writeCSV('willwatch.csv', isMain)
+    try:
+        url = baseURL + "?type=" + ("main" if isMain else "willwatch")
+        response = requests.delete(f"{url}&number={index}")
+    except requests.exceptions.RequestException as e:
+        print(f"Ошибка при подключении к серверу: {e}")
+        return []
 
 @eel.expose
-def find_anime(name):
-    with open('data.csv', 'r', newline='', encoding='utf-8') as csvfile:
-        reader = csv.DictReader(csvfile)
-        matching_anime = []
-        for row in reader:
-            if re.search(name, row['Name'], re.IGNORECASE):
-                matching_anime.append(row)
-        return matching_anime
+def find_anime(name, isMain):
+    matching_anime = []
+    for anime in anime_list:
+        if re.search(name, anime['Name'], re.IGNORECASE):
+            matching_anime.append(anime)
+    return matching_anime
 
-def writeCSV(CSVName, isMain):
-    with open(CSVName, 'w', newline='', encoding='utf-8') as csvfile:
-        fieldnames = ['Name', 'Progress', 'ProgressType', 'Notes'] if isMain else ['Name', 'Notes']
-        writer = csv.DictWriter(csvfile, fieldnames=fieldnames)
-        writer.writeheader()
-        for anime in anime_list:
-            writer.writerow(anime)
+# @eel.expose
+# def find_anime(name):
+#     with open('data.csv', 'r', newline='', encoding='utf-8') as csvfile:
+#         reader = csv.DictReader(csvfile)
+#         matching_anime = []
+#         for anime in reader:
+#             if re.search(name, anime['Name'], re.IGNORECASE):
+#                 matching_anime.append(anime)
+#         return matching_anime
 
-eel.start('index.html', size=(1300, 900))
+# def writeCSV(CSVName, isMain):
+#     with open(CSVName, 'w', newline='', encoding='utf-8') as csvfile:
+#         fieldnames = ['Name', 'Progress', 'ProgressType', 'Notes'] if isMain else ['Name', 'Notes']
+#         writer = csv.DictWriter(csvfile, fieldnames=fieldnames)
+#         writer.writeheader()
+#         for anime in anime_list:
+#             writer.writerow(anime)
+
+#eel.start('index.html', size=(1300, 900))
+eel.start('index.html', size=(1300, 900), mode='my_portable_chromium')
--- web/script.js
+++ web/script.js
@@ -1,5 +1,6 @@
 const mainPage =  document.getElementById('main-page');
 const animeCount =  document.getElementById('anime-count');
+let progressTypePrev = 'сезоны'
 
 function loadMainPage(){
     mainPage.innerHTML = 
@@ -23,29 +24,38 @@ function loadMainPage(){
     <input class="input" type='button' id='anim-add-btn' value='Добавить' onclick="addAnime">
     <input class="input" type='text' id='search' placeholder='Поиск'>
     <div id="anime-list"></div>
-    <div id="willwatch">
+    <div id="willwatch" class="openSomethingBtn">
         <svg width="30px" height="30px" viewBox="0 0 24 24" fill="#ffffff">
             <path d="M5 6.2C5 5.07989 5 4.51984 5.21799 4.09202C5.40973 3.71569 5.71569 3.40973 6.09202 3.21799C6.51984 3 7.07989 3 8.2 3H15.8C16.9201 3 17.4802 3 17.908 3.21799C18.2843 3.40973 18.5903 3.71569 18.782 4.09202C19 4.51984 19 5.07989 19 6.2V21L12 16L5 21V6.2Z" stroke="#ffffff" stroke-width="2" stroke-linejoin="round"/>
         </svg>
         <p>Смотреть позже</p>
+    </div>
+    <div id="settings" class="openSomethingBtn">
+        <svg fill="#ffffff" height="200px" width="200px" version="1.1" id="Capa_1" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" viewBox="0 0 54 54" xml:space="preserve" stroke="#ffffff">
+            <g id="SVGRepo_bgCarrier" stroke-width="0"></g><g id="SVGRepo_tracerCarrier" stroke-linecap="round" stroke-linejoin="round"></g><g id="SVGRepo_iconCarrier"> <g> <path d="M51.22,21h-5.052c-0.812,0-1.481-0.447-1.792-1.197s-0.153-1.54,0.42-2.114l3.572-3.571 c0.525-0.525,0.814-1.224,0.814-1.966c0-0.743-0.289-1.441-0.814-1.967l-4.553-4.553c-1.05-1.05-2.881-1.052-3.933,0l-3.571,3.571 c-0.574,0.573-1.366,0.733-2.114,0.421C33.447,9.313,33,8.644,33,7.832V2.78C33,1.247,31.753,0,30.22,0H23.78 C22.247,0,21,1.247,21,2.78v5.052c0,0.812-0.447,1.481-1.197,1.792c-0.748,0.313-1.54,0.152-2.114-0.421l-3.571-3.571 c-1.052-1.052-2.883-1.05-3.933,0l-4.553,4.553c-0.525,0.525-0.814,1.224-0.814,1.967c0,0.742,0.289,1.44,0.814,1.966l3.572,3.571 c0.573,0.574,0.73,1.364,0.42,2.114S8.644,21,7.832,21H2.78C1.247,21,0,22.247,0,23.78v6.439C0,31.753,1.247,33,2.78,33h5.052 c0.812,0,1.481,0.447,1.792,1.197s0.153,1.54-0.42,2.114l-3.572,3.571c-0.525,0.525-0.814,1.224-0.814,1.966 c0,0.743,0.289,1.441,0.814,1.967l4.553,4.553c1.051,1.051,2.881,1.053,3.933,0l3.571-3.572c0.574-0.573,1.363-0.731,2.114-0.42 c0.75,0.311,1.197,0.98,1.197,1.792v5.052c0,1.533,1.247,2.78,2.78,2.78h6.439c1.533,0,2.78-1.247,2.78-2.78v-5.052 c0-0.812,0.447-1.481,1.197-1.792c0.751-0.312,1.54-0.153,2.114,0.42l3.571,3.572c1.052,1.052,2.883,1.05,3.933,0l4.553-4.553 c0.525-0.525,0.814-1.224,0.814-1.967c0-0.742-0.289-1.44-0.814-1.966l-3.572-3.571c-0.573-0.574-0.73-1.364-0.42-2.114 S45.356,33,46.168,33h5.052c1.533,0,2.78-1.247,2.78-2.78V23.78C54,22.247,52.753,21,51.22,21z M52,30.22 C52,30.65,51.65,31,51.22,31h-5.052c-1.624,0-3.019,0.932-3.64,2.432c-0.622,1.5-0.295,3.146,0.854,4.294l3.572,3.571 c0.305,0.305,0.305,0.8,0,1.104l-4.553,4.553c-0.304,0.304-0.799,0.306-1.104,0l-3.571-3.572c-1.149-1.149-2.794-1.474-4.294-0.854 c-1.5,0.621-2.432,2.016-2.432,3.64v5.052C31,51.65,30.65,52,30.22,52H23.78C23.35,52,23,51.65,23,51.22v-5.052 c0-1.624-0.932-3.019-2.432-3.64c-0.503-0.209-1.021-0.311-1.533-0.311c-1.014,0-1.997,0.4-2.761,1.164l-3.571,3.572 c-0.306,0.306-0.801,0.304-1.104,0l-4.553-4.553c-0.305-0.305-0.305-0.8,0-1.104l3.572-3.571c1.148-1.148,1.476-2.794,0.854-4.294 C10.851,31.932,9.456,31,7.832,31H2.78C2.35,31,2,30.65,2,30.22V23.78C2,23.35,2.35,23,2.78,23h5.052 c1.624,0,3.019-0.932,3.64-2.432c0.622-1.5,0.295-3.146-0.854-4.294l-3.572-3.571c-0.305-0.305-0.305-0.8,0-1.104l4.553-4.553 c0.304-0.305,0.799-0.305,1.104,0l3.571,3.571c1.147,1.147,2.792,1.476,4.294,0.854C22.068,10.851,23,9.456,23,7.832V2.78 C23,2.35,23.35,2,23.78,2h6.439C30.65,2,31,2.35,31,2.78v5.052c0,1.624,0.932,3.019,2.432,3.64 c1.502,0.622,3.146,0.294,4.294-0.854l3.571-3.571c0.306-0.305,0.801-0.305,1.104,0l4.553,4.553c0.305,0.305,0.305,0.8,0,1.104 l-3.572,3.571c-1.148,1.148-1.476,2.794-0.854,4.294c0.621,1.5,2.016,2.432,3.64,2.432h5.052C51.65,23,52,23.35,52,23.78V30.22z"></path> <path d="M27,18c-4.963,0-9,4.037-9,9s4.037,9,9,9s9-4.037,9-9S31.963,18,27,18z M27,34c-3.859,0-7-3.141-7-7s3.141-7,7-7 s7,3.141,7,7S30.859,34,27,34z"></path> </g> </g>
+        </svg>
+        <p>Настройки</p>
     </div>`
     
     const addBtn =  document.getElementById('anim-add-btn');
     const search =  document.getElementById('search');
     const willWatchBtn =  document.getElementById('willwatch');
+    const settingsBtn =  document.getElementById('settings');
     const animeListDiv = document.getElementById('anime-list');
     const progressType = document.querySelectorAll('input[name="progressType"]')
     const animeName = document.getElementById("anime-name")
     const progress = document.getElementById("anime-progress")
     const notes = document.getElementById("anime-notes")
-    let progressTypePrev = 'сезоны'
-    addBtn.addEventListener('click', {handleEvent: addAnime, progressTypePrev: progressTypePrev, progress: progress, animeName: animeName, notes: notes, isMain: true, search: search, animeListDiv: animeListDiv, animeCount: animeCount})
+   
+    addBtn.addEventListener('click', {handleEvent: addAnime, progress: progress, animeName: animeName, notes: notes, isMain: true, search: search, animeListDiv: animeListDiv, animeCount: animeCount})
     search.addEventListener('input', {handleEvent: loadAnimeList, isSearch: true, search: search, animeListDiv: animeListDiv, isMain: true}) 
     willWatchBtn.addEventListener('click', loadWillWatchPage)
+    settingsBtn.addEventListener('click', loadSettingPage)
     
     for (var i = 0; i < progressType.length; i++) {
         progressType[i].addEventListener('change', function() {
-            if (this !== progressTypePrev) {
+            console.log(this.value)
+            if (this.value !== progressTypePrev) {
                 progressTypePrev = this.value;
             }
         })
@@ -62,48 +72,79 @@ function loadWillWatchPage() {
     <input class="input" type='button' id='anim-add-btn' value='Добавить' onclick="addAnime">
     <input class="input" type='text' id='search' placeholder='Поиск'>
     <div id="anime-list"></div>
-    <div id="willwatch">
+    <div id="willwatch" class="openSomethingBtn">
         <svg width="30px" height="30px" viewBox="0 0 24 24" fill="#ffffff">
             <path d="M5 6.2C5 5.07989 5 4.51984 5.21799 4.09202C5.40973 3.71569 5.71569 3.40973 6.09202 3.21799C6.51984 3 7.07989 3 8.2 3H15.8C16.9201 3 17.4802 3 17.908 3.21799C18.2843 3.40973 18.5903 3.71569 18.782 4.09202C19 4.51984 19 5.07989 19 6.2V21L12 16L5 21V6.2Z" stroke="#ffffff" stroke-width="2" stroke-linejoin="round"/>
         </svg>
         <p>Просмотренные аниме</p>
+    </div>
+    <div id="settings" class="openSomethingBtn">
+        <svg fill="#ffffff" height="200px" width="200px" version="1.1" id="Capa_1" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" viewBox="0 0 54 54" xml:space="preserve" stroke="#ffffff">
+            <g id="SVGRepo_bgCarrier" stroke-width="0"></g><g id="SVGRepo_tracerCarrier" stroke-linecap="round" stroke-linejoin="round"></g><g id="SVGRepo_iconCarrier"> <g> <path d="M51.22,21h-5.052c-0.812,0-1.481-0.447-1.792-1.197s-0.153-1.54,0.42-2.114l3.572-3.571 c0.525-0.525,0.814-1.224,0.814-1.966c0-0.743-0.289-1.441-0.814-1.967l-4.553-4.553c-1.05-1.05-2.881-1.052-3.933,0l-3.571,3.571 c-0.574,0.573-1.366,0.733-2.114,0.421C33.447,9.313,33,8.644,33,7.832V2.78C33,1.247,31.753,0,30.22,0H23.78 C22.247,0,21,1.247,21,2.78v5.052c0,0.812-0.447,1.481-1.197,1.792c-0.748,0.313-1.54,0.152-2.114-0.421l-3.571-3.571 c-1.052-1.052-2.883-1.05-3.933,0l-4.553,4.553c-0.525,0.525-0.814,1.224-0.814,1.967c0,0.742,0.289,1.44,0.814,1.966l3.572,3.571 c0.573,0.574,0.73,1.364,0.42,2.114S8.644,21,7.832,21H2.78C1.247,21,0,22.247,0,23.78v6.439C0,31.753,1.247,33,2.78,33h5.052 c0.812,0,1.481,0.447,1.792,1.197s0.153,1.54-0.42,2.114l-3.572,3.571c-0.525,0.525-0.814,1.224-0.814,1.966 c0,0.743,0.289,1.441,0.814,1.967l4.553,4.553c1.051,1.051,2.881,1.053,3.933,0l3.571-3.572c0.574-0.573,1.363-0.731,2.114-0.42 c0.75,0.311,1.197,0.98,1.197,1.792v5.052c0,1.533,1.247,2.78,2.78,2.78h6.439c1.533,0,2.78-1.247,2.78-2.78v-5.052 c0-0.812,0.447-1.481,1.197-1.792c0.751-0.312,1.54-0.153,2.114,0.42l3.571,3.572c1.052,1.052,2.883,1.05,3.933,0l4.553-4.553 c0.525-0.525,0.814-1.224,0.814-1.967c0-0.742-0.289-1.44-0.814-1.966l-3.572-3.571c-0.573-0.574-0.73-1.364-0.42-2.114 S45.356,33,46.168,33h5.052c1.533,0,2.78-1.247,2.78-2.78V23.78C54,22.247,52.753,21,51.22,21z M52,30.22 C52,30.65,51.65,31,51.22,31h-5.052c-1.624,0-3.019,0.932-3.64,2.432c-0.622,1.5-0.295,3.146,0.854,4.294l3.572,3.571 c0.305,0.305,0.305,0.8,0,1.104l-4.553,4.553c-0.304,0.304-0.799,0.306-1.104,0l-3.571-3.572c-1.149-1.149-2.794-1.474-4.294-0.854 c-1.5,0.621-2.432,2.016-2.432,3.64v5.052C31,51.65,30.65,52,30.22,52H23.78C23.35,52,23,51.65,23,51.22v-5.052 c0-1.624-0.932-3.019-2.432-3.64c-0.503-0.209-1.021-0.311-1.533-0.311c-1.014,0-1.997,0.4-2.761,1.164l-3.571,3.572 c-0.306,0.306-0.801,0.304-1.104,0l-4.553-4.553c-0.305-0.305-0.305-0.8,0-1.104l3.572-3.571c1.148-1.148,1.476-2.794,0.854-4.294 C10.851,31.932,9.456,31,7.832,31H2.78C2.35,31,2,30.65,2,30.22V23.78C2,23.35,2.35,23,2.78,23h5.052 c1.624,0,3.019-0.932,3.64-2.432c0.622-1.5,0.295-3.146-0.854-4.294l-3.572-3.571c-0.305-0.305-0.305-0.8,0-1.104l4.553-4.553 c0.304-0.305,0.799-0.305,1.104,0l3.571,3.571c1.147,1.147,2.792,1.476,4.294,0.854C22.068,10.851,23,9.456,23,7.832V2.78 C23,2.35,23.35,2,23.78,2h6.439C30.65,2,31,2.35,31,2.78v5.052c0,1.624,0.932,3.019,2.432,3.64 c1.502,0.622,3.146,0.294,4.294-0.854l3.571-3.571c0.306-0.305,0.801-0.305,1.104,0l4.553,4.553c0.305,0.305,0.305,0.8,0,1.104 l-3.572,3.571c-1.148,1.148-1.476,2.794-0.854,4.294c0.621,1.5,2.016,2.432,3.64,2.432h5.052C51.65,23,52,23.35,52,23.78V30.22z"></path> <path d="M27,18c-4.963,0-9,4.037-9,9s4.037,9,9,9s9-4.037,9-9S31.963,18,27,18z M27,34c-3.859,0-7-3.141-7-7s3.141-7,7-7 s7,3.141,7,7S30.859,34,27,34z"></path> </g> </g>
+        </svg>
+        <p>Настройки</p>
     </div>`
 
     const addBtn =  document.getElementById('anim-add-btn');
     const search =  document.getElementById('search');
     const willWatchBtn =  document.getElementById('willwatch');
     const animeListDiv = document.getElementById('anime-list');
-    const progressType = document.querySelectorAll('input[name="progressType"]')
     const animeName = document.getElementById("anime-name")
     const notes = document.getElementById("anime-notes")
-    let progressTypePrev = 'сезоны'
-    addBtn.addEventListener('click', {handleEvent: addAnime, progressTypePrev: progressTypePrev, isMain: false, search: search, animeListDiv: animeListDiv, animeName: animeName, notes: notes})
+    const settingsBtn =  document.getElementById('settings');
+
+    addBtn.addEventListener('click', {handleEvent: addAnime, isMain: false, search: search, animeListDiv: animeListDiv, animeName: animeName, notes: notes})
     search.addEventListener('input', {handleEvent: loadAnimeList, isSearch: true, search: search, animeListDiv: animeListDiv, isMain: false})
     willWatchBtn.addEventListener('click', loadMainPage)
+    settingsBtn.addEventListener('click', loadSettingPage)
     
-    for (var i = 0; i < progressType.length; i++) {
-        progressType[i].addEventListener('change', function() {
-            if (this !== progressTypePrev) {
-                progressTypePrev = this.value;
-            }
-        })
-    }
     loadAnimeList.call({ isSearch: false, animeListDiv: animeListDiv, isMain: false, animeCount: animeCount });
+}
+
+async function loadSettingPage() {
+    mainPage.innerHTML = `//<div class="anime-text">Сохранять локально?</div>
+    // <div id='anime-radio'>
+    //     <input type="radio" id="yes" name="localSaving" value="Да" checked />
+    //     <label for="yes">Да</label>
+    //     <input type="radio" id="no" name="localSaving" value="Нет" />
+    //     <label for="no">Нет</label>
+    // </div>
+    <input class="input" type='text' id='server' placeholder='http://192.168.31.100:22848/index.php'>
+    <div id="main" class="openSomethingBtn">
+        <svg fill="#fff" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg" id="home-alt-3" class="icon glyph" stroke="#fff"><g id="SVGRepo_bgCarrier" stroke-width="0">
+            </g><g id="SVGRepo_tracerCarrier" stroke-linecap="round" stroke-linejoin="round"></g><g id="SVGRepo_iconCarrier"><path d="M21.71,10.29l-9-9a1,1,0,0,0-1.42,0l-9,9a1,1,0,0,0-.21,1.09A1,1,0,0,0,3,12H4v9a1,1,0,0,0,1,1H8a1,1,0,0,0,1-1V15a1,1,0,0,1,1-1h4a1,1,0,0,1,1,1v6a1,1,0,0,0,1,1h3a1,1,0,0,0,1-1V12h1a1,1,0,0,0,.92-.62A1,1,0,0,0,21.71,10.29Z"></path></g>
+        </svg>
+        <p>Вернутся на главную</p>
+    </div>`
+    const mainBtn =  document.getElementById('main');
+    const server = document.getElementById("server");
+    const baseURL = await eel.getConfigDate("baseURL")();
+
+    if (baseURL.length >= 10) {
+        server.value = baseURL
+    }
+
+    mainBtn.addEventListener('click', loadMainPage);
+    server.addEventListener('input', async function(event) {
+        await eel.setConfigDate("baseURL", server.value)();
+        console.log('Значение изменено на: ', event.target.value);
+    });
 }
 
 function addAnime() {
     if(this.isMain) {
-        if (this.progressTypePrev === "не применимо" && this.progress.value != "")
+        console.log(progressTypePrev)
+        if (progressTypePrev === "не применимо" && this.progress.value != "")
         return
         
-        if (this.progressTypePrev != "не применимо" && this.progress.value === "")
+        if ((progressTypePrev === "сезоны" || progressTypePrev === "серии") && this.progress.value === "")
             return
 
         if(this.animeName.value){
             eel.add_anime({
                 'Name': this.animeName.value, 
                 'Progress': this.progress.value ? this.progress.value : 'n/d', 
-                'ProgressType': this.progressTypePrev, 
+                'ProgressType': progressTypePrev, 
                 'Notes': this.notes.value ? this.notes.value : 'n/d'
             }, this.isMain)()
             this.animeName.value = ''
@@ -127,9 +168,10 @@ function addAnime() {
 }
 
 async function loadAnimeList() {
+    console.log('loadAnimeList')
     this.animeListDiv.innerHTML = ''
     try {
-        let arr = this.isSearch ? await eel.find_anime(search.value)() : (this.isMain ? await eel.load_main_csv()() : await eel.load_willWatch_csv()());
+        let arr = this.isSearch ? await eel.find_anime(search.value, this.isMain)() : (this.isMain ? await eel.load_main_csv()() : await eel.load_willWatch_csv()());
         arr.reverse().forEach((anime, index) => {
             let animeItemDiv = document.createElement('div');
             animeItemDiv.classList.add('anime-list-item');
@@ -150,7 +192,7 @@ async function loadAnimeList() {
             deleteButton.textContent = 'X';
             deleteButton.classList.add('delete-button');
             deleteButton.addEventListener('click', (event) => {
-                eel.delete_anime(arr.length-index-1, this.isMain)()
+                eel.delete_anime(arr.length - index - 1, this.isMain)()
                 event.target.parentElement.remove();
             });
 
